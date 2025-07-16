2050559e91a30530be87834a7f2608778e8bd425
Zacck <coderv63@gmail.com>
2025-07-16 13:37:20 +0200

    Initialize some indicators to show progress of what is happening

--- Src/main.c
+++ Src/main.c
@@ -20,22 +20,31 @@
 #include <stdint.h>
 #include "stm32f4xx.h"
 
-#define BLUE_LED_PORT GPIOD
-#define BLUE_LED_PIN 15
 
 
-void _init(void){
+void _init() {
 	//dummy
 }
 
+
+void init_indicators(); 
 
 
 
 int main(void)
 {
-	//Turn on Blue LED with pure CMSIS
+	init_indicators();
+	
+    /* Loop forever */
+	for(;;);
+}
+
+
+void init_indicators() 
+{
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
+	//BLUE
 	GPIOD->MODER |= GPIO_MODER_MODER15_0;
 	GPIOD->MODER &= ~GPIO_MODER_MODER15_1;
 	GPIOD->OTYPER &= ~GPIO_OTYPER_OT_15;
@@ -45,11 +54,24 @@ int main(void)
 	GPIOD->PUPDR &= ~GPIO_PUPDR_PUPDR15_1;
 
 
+	//RED
+	GPIOD->MODER |= GPIO_MODER_MODER14_0;
+	GPIOD->MODER &= ~GPIO_MODER_MODER14_1;
+	GPIOD->OTYPER &= ~GPIO_OTYPER_OT_14;
+	GPIOD->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR14_0;
+	GPIOD->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR14_1;
+	GPIOD->PUPDR &= ~GPIO_PUPDR_PUPDR14_0;
+	GPIOD->PUPDR &= ~GPIO_PUPDR_PUPDR14_1;
+
+	//ORANGE
+	GPIOD->MODER |= GPIO_MODER_MODER13_0;
+	GPIOD->MODER &= ~GPIO_MODER_MODER13_1;
+	GPIOD->OTYPER &= ~GPIO_OTYPER_OT_13;
+	GPIOD->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR13_0;
+	GPIOD->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR13_1;
+	GPIOD->PUPDR &= ~GPIO_PUPDR_PUPDR13_0;
+	GPIOD->PUPDR &= ~GPIO_PUPDR_PUPDR13_1;
 
 
-	GPIOD->BSRR |= GPIO_BSRR_BS_15;
-
-
-    /* Loop forever */
-	for(;;);
 }
+
